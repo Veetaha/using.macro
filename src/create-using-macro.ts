90ee1276280ecb1907930c7d6ff9dfb0b317a3ae
Veetaha <gerzoh1@gmail.com>
2019-10-03 04:07:24 +0300

    feat(readme,api): added documentation, added ability to return statements and arrays of statements from destructor factory

--- src/create-using-macro.ts
+++ src/create-using-macro.ts
@@ -9,7 +9,7 @@ import { NodePath    } from '@babel/traverse';
 import { getScopeBlock } from './get-scope-block';
 import { removeAndNormalizeUsingCallExpr } from './remove-and-normalize-using-call-expr';
 
-export type DestructingStFactory = (garbageId: NodePath<T.Identifier>) => T.Expression | T.Statement | T.Statement[];
+export type DestructingCodeFactory = (garbageId: NodePath<T.Identifier>) => T.Expression | T.Statement | T.Statement[];
 
 /**
  * Creates a macro function for `'babel-plugin-macros'` that implements the logic
@@ -20,7 +20,7 @@ export type DestructingStFactory = (garbageId: NodePath<T.Identifier>) => T.Expr
  *                             of statements that implement destructing logic for it
  *                             that will be added into `finally {}` block.
  */
-export function createUsingMacro(createDestructingCode: DestructingStFactory): MacroFn {
+export function createUsingMacro(createDestructingCode: DestructingCodeFactory): MacroFn {
     return ({references}) => {
         for (const refName in own(references))
         // Iterating backwards in order not to affect other using bindings declared within
@@ -71,7 +71,7 @@ function toStatementArray(val: T.Expression | T.Statement | T.Statement[]) {
  * @param functionName Name of the function that will be called on garbage objects
  *                     in output code.
  */
-export function createFunctionCallFactory(functionName: string): DestructingStFactory {
+export function createFunctionCallFactory(functionName: string): DestructingCodeFactory {
     return garbageId => T.callExpression(
         T.identifier(functionName), [garbageId.node]
     );
@@ -82,7 +82,7 @@ export function createFunctionCallFactory(functionName: string): DestructingStFa
  * @param functionName Name of the method that will be called on garbage objects
  *                     in output code.
  */
-export function createMethodCallFactory(methodName: string): DestructingStFactory {
+export function createMethodCallFactory(methodName: string): DestructingCodeFactory {
     return garbageId => T.callExpression(
         T.memberExpression(garbageId.node, T.identifier(methodName)), []
     );
